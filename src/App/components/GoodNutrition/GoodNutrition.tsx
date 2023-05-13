13cdf5ceb4979f16ead80fd1391180e59563576f
mobacsell <mobacsell@yandex.ru>
2023-05-13 19:26:24 +0300

    feat: add adaptivity in the GoodNutrition component

--- src/App/components/GoodNutrition/GoodNutrition.tsx
+++ src/App/components/GoodNutrition/GoodNutrition.tsx
@@ -2,27 +2,33 @@ import styles from "./GoodNutrition.module.css";
 import image from "../../../assets/goodNutrition/nutritionist.png";
 
 export function GoodNutrition() {
+  const itemList = [
+    "Здоровый ребенок",
+    "Повышение иммунитета",
+    "Поддержание фигуры",
+    "Отстутствие вредных компонентов",
+    "Снижение нагрузки на органы пищеварения",
+  ];
+
   return (
     <section className={styles.root}>
       <div className={styles.wrapper}>
-        <img src={image} alt="женщина с овощами" className={styles.image} />
-        <div className={styles.content}>
-          <h1 className={styles.header}>
-            Почему Вам нужно правильно питаться?
-          </h1>
-          <ul className={styles.list}>
-            <li className={styles.item}>Здоровый ребенок</li>
-            <li className={styles.item}>Повышение иммунитета</li>
-            <li className={styles.item}>Поддержание фигуры</li>
-            <li className={styles.item}>Отстутствие вредных компонентов </li>
-            <li className={styles.item}>
-              Снижение нагрузки на органы пищеварения
-            </li>
-          </ul>
+        <div className={styles.imageWrapper}>
+          <img src={image} alt="женщина с овощами" className={styles.image} />
+          <div className={styles.imageDescription}>
+            <h3 className={styles.imageTitle}>Иванова Надежда Александровна</h3>
+            <p className={styles.imageText}>Сертифицированный диетолог</p>
+          </div>
         </div>
-        <div className={styles.description}>
-          <h1 className={styles.name}>Иванова Надежда Александровна</h1>
-          <p className={styles.paragraph}>Сертифицированный диетолог</p>
+        <div className={styles.content}>
+          <h2 className={styles.header}>
+            Почему Вам нужно правильно питаться?
+          </h2>
+          <ul className={styles.list}>
+            {itemList.map((value, index) => {
+              return <li className={styles.item} key={`item_${index}`}>{value}</li>;
+            })}
+          </ul>
         </div>
       </div>
     </section>
