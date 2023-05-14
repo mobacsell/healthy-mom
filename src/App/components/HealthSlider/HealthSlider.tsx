0b28044471dc06669e7b1dc4f1e2b38a64ed6db6
mobacsell <mobacsell@yandex.ru>
2023-05-14 16:20:33 +0300

    feat: add adaptivity to the HealthSlider comp

--- src/App/components/HealthSlider/HealthSlider.tsx
+++ src/App/components/HealthSlider/HealthSlider.tsx
@@ -39,38 +39,72 @@ export function HealthSlider() {
     },
   ];
 
+  /**
+   * Настройки для слайдера React Slick:
+   * infinite - бесконечный слайдер
+   * centerMode, centerPadding - текущий слайдер по центру, боковые паддинги для слайдера
+   * sliderToShow - количество слайдов для отображения
+   * speed - скорость слайдера
+   * swipeToSlide - возможность перематывать слайдер мышкой
+   * responsive - адаптация слайдера под разрешение экрана (breakpoint)
+   */
   const settings = {
     infinite: true,
+    centerMode: true,
+    centerPadding: "0px",
     slidesToShow: 3,
-    speed: 400,
+    speed: 700,
     swipeToSlide: true,
-    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          infinite: true,
+          centerPadding: "110px",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          centerPadding: "70px",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          infinite: true,
+          centerPadding: "30px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <div className={styles.root}>
-      <div className={styles.wrapper}>
-        <h2 className={styles.header}>
-          Обязательные продукты для здорового питания:
-        </h2>
-        <Slider {...settings}>
-          {dataSlider.map((value) => {
-            return (
-              <div className={styles.itemWrapper}>
-                <div className={styles.item}>
-                  <img
-                    src={value.url}
-                    alt={value.header}
-                    className={styles.itemImage}
-                  />
-                  <h3 className={styles.itemHeader}>{value.header}</h3>
-                  <p className={styles.itemText}>{value.text}</p>
-                </div>
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
-    </div>
+    <section className={styles.root}>
+      <h2 className={styles.header}>
+        Обязательные продукты
+        <br />
+        для здорового питания:
+      </h2>
+      <Slider {...settings}>
+        {dataSlider.map((value) => {
+          return (
+            <div className={styles.item}>
+              <img
+                src={value.url}
+                alt={value.header}
+                className={styles.itemImage}
+              />
+              <h3 className={styles.itemHeader}>{value.header}</h3>
+              <p className={styles.itemText}>{value.text}</p>
+            </div>
+          );
+        })}
+      </Slider>
+    </section>
   );
 }
